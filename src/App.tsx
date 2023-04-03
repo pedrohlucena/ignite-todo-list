b0bd9c1bcbe430ff652bd8d39e29aff23050c8db
Pedro H. Lucena <pedrohlucena413@gmail.com>
2023-04-02 21:19:33 -0300

    create input element

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { Header } from "./components/Header/Header";
+import styles from "./App.module.css";
 import "./global.css";
+import { Input } from "./components/Input/Input";
 
 export function App() {
   return (
     <div>
       <Header />
+      <div className={styles.container}>
+        <div className={styles.main}>
+          <Input placeholder="Adicione uma nova tarefa" />
+        </div>
+      </div>
     </div>
   );
 }
